5d1154c45b364708157602abe2414f5f17dc1ddd
nafis <mahfuznafis1@gmail.com>
2025-01-19 16:02:37 +0600

    Implemented funcion add_server

--- consistent_hashing.cpp
+++ consistent_hashing.cpp
@@ -8,6 +8,7 @@ int hash_string(string value);
 void add_server(string server_name);
 void add_key(string key);
 void remove_server(string server_name);
+void print_server_keys(string server_name);
 
 
 void empty_hash_ring()
@@ -33,6 +34,8 @@ int main()
     add_server("ser_2");
     add_key("three");
     add_key("four");
+
+    add_server("130");
 }
 
 int hash_string(string value)
@@ -65,10 +68,89 @@ int hash_string(string value)
 void add_server(string server_name)
 {
     int hash_value = hash_string(server_name);
-
     hash_ring[hash_value] = {server_name,2025};
 
     cout<<"Server added successfully!\nHash value: "<<hash_value<<endl;
+
+    vector<pair<string,int> > temporary_key_holder;
+    string previous_server;
+
+    for(int i = hash_value - 1; ; i--)
+    {
+        if(i == -1)
+            i = 508;
+
+        if(i == hash_value)
+        {
+            cout<<"\nThere is only one server in the server pool.\n";
+            cout<<"No need for key redistribution\n";
+
+            break;
+        }
+
+        if(hash_ring[i].second== 2025)
+        {
+            previous_server = hash_ring[i].first;
+            cout<<"Previous Server Found! Server name: <"<<previous_server<<">\n\n";
+            cout<<"This server contains: ";
+
+            print_server_keys(previous_server);
+            break;
+        }
+
+        if(hash_ring[i].second != -1)
+        {
+            temporary_key_holder.push_back(hash_ring[i]);
+        }
+    }
+
+    if(temporary_key_holder.size() != 0)
+    {   
+        cout<<"--------------------------------------------\n";
+        cout<<"Total keys to be transferred: "<<temporary_key_holder.size()<<endl;
+
+        for(int i = 0; i < temporary_key_holder.size(); i++)
+            cout<<"<"<<temporary_key_holder[i].first<<">    ";
+        cout<<"\n****************************************************\n";
+
+        for(int i = 0; i < temporary_key_holder.size(); i++)
+        {
+
+            string current_key = temporary_key_holder[i].first;
+            int current_key_server_hash = temporary_key_holder[i].second;
+            string current_key_server_name = hash_ring[current_key_server_hash].first;
+
+            cout<< "Key: <"<<current_key<<">\tPrevious Server: <"<<current_key_server_name<<">\n";
+        
+            int key_hash = hash_string(current_key);
+            hash_ring[key_hash] = {current_key, hash_value};
+            server_pool[server_name].push_back(current_key);
+
+            cout<<"\nKey transferred to new server successfully! Now:\n";
+            cout<< "Key: <"<<current_key<<">\tCurrent server: <"<<hash_ring[hash_ring[key_hash].second].first<<">\n";
+
+            cout<<"\nBefore Deleting from Key's Previous Server, it contains: ";
+            print_server_keys(current_key_server_name);
+            
+            auto it = server_pool.find(current_key_server_name);
+
+            if(it != server_pool.end())
+            {
+                auto &vec = it->second;
+                vec.erase(remove(vec.begin(), vec.end(), current_key), vec.end());
+            }
+
+            cout<<"\nAfter Deleting from Key's Previous Server, it contains: ";
+            print_server_keys(current_key_server_name);
+        }
+        cout<<"--------------------------------------------\n\n";
+    }
+
+    cout<<"\n****************************************************\n";
+    cout<<"The added server now contains: ";
+    print_server_keys(server_name);
+    cout<<"\n****************************************************\n";
+
 }
 
 void add_key(string key)
@@ -111,4 +193,15 @@ void add_key(string key)
 void remove_server(string server_name)
 {
 
+}
+
+void print_server_keys(string server_name)
+{
+    vector<string> keys = server_pool[server_name];
+
+    for(int i = 0; i < keys.size(); i++)
+        cout<<"<"<<keys[i]<<">\t";
+
+    cout<<endl<<endl;
+    
 }
